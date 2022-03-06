e0d7e05be40e2533674ed0ce0df194c4446ecae9
Vansh Trivedi <vansh.trivedi_cs.aiml19@gla.ac.in>
2022-03-06 06:59:19 -0800

    intial Commit

---
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<RecyclerView>(R.id.recyclerView).layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this,this)
+        findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
+
+        viewModel = ViewModelProvider(this,
+          ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this, Observer {list ->
+           list?.let {
+               adapter.updateList(it)
+           }
+        })
+
+
+    }
+
+    fun onItemClicked(note: Note) {
+        viewModel.deleteNote(note)
+        Toast.makeText(this, "${note.text} Deleted",Toast.LENGTH_LONG).show()
+    }
+
+    fun submitData(view: View) {
+        val noteText = findViewById<EditText>(R.id.input).text.toString()
+        if(noteText.isNotEmpty()){
+            viewModel.insertNote(Note(noteText))
+            Toast.makeText(this, "$noteText Inserted",Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notes/NotesRVAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.notes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesRVAdapter(private val context: Context, private val listener: MainActivity): RecyclerView.Adapter<NotesRVAdapter.NoteViewholder>() {
+
+    val allNotes = ArrayList<Note>()
+
+    inner class NoteViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val  textView = itemView.findViewById<TextView>(R.id.text)
+        val deleteButton = itemView.findViewById<ImageView>(R.id.deleteButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewholder {
+       val viewHolder =NoteViewholder(LayoutInflater.from(context).inflate(R.layout.item_note,parent, false))
+        viewHolder.deleteButton.setOnClickListener{
+            listener.onItemClicked(allNotes[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: NoteViewholder, position: Int) {
+        val currentNote = allNotes[position]
+        holder.textView.text = currentNote.text
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+
+    fun updateList(newList: List<Note>) {
+        allNotes.clear()
+        allNotes.addAll(newList)
+
+        notifyDataSetChanged()
+    }
+
+}
+
+interface INotesRVAdapter {
+    fun onItemClicked(note: Note)
+}
